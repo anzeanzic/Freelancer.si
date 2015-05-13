31030bacdc58995a99a663d5a59c92b11df83eb9
lostje <anze.anzic@siol.net>
2015-05-14 00:12:22 +0200

    - Added global initilization of form presence flags

--- freelancer.js
+++ freelancer.js
@@ -1,15 +1,10 @@
-/*
-1 - izdelava spletnih strani
-2 - povpraševanje po angularjs
-3 - povpraševanje po svetovanju (textarea)
-
-email - valid, req tel. št., poštno št., ime pa priimek
-
-*/
-
 angular.module('app', []);
 angular.module('app').controller('AppController', function($scope) {
+	$scope.webpage = false;
+	$scope.angular = false;
+	$scope.query = false;
 	$scope.choices = [{ name: "Spletna stran", id: 1}, { name: "AngularJS", id: 2}, { name: "Svetovanje", id: 3}];
+
 	$scope.$watch('selectedObject', function(newValue) {
 		if (newValue != undefined) {
 			$scope.webpage = false;
@@ -34,13 +29,3 @@ angular.module('app').controller('AppController', function($scope) {
 		alert("Obrazec uspesno oddan!")
 	}
 });
-
-
-/*
-Domaca naloga:
-Uporabnik vpise datum odhoda, prihoda (navadne stevilke lahko), mora izbrati letalisce (maribor, lj, gradec), in pa kam hoce pride () in pa osebne podatke (email, ime, naslov). Ko izbere prihod in odhod,
-mu prikaze ceno.
-
-Do petka do tecaja
-
-*/
